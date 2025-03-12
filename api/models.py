fb12890458bbf87e472c4979bd34fb13d0642c17
phanupan <81475304+phanupan@users.noreply.github.com>
2025-03-12 10:24:00 +0700

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,24 @@
+from datetime import timezone
+from django.db import models
+
+# Create your models here.
+
+
+class Store(models.Model):
+    store_id = models.IntegerField(unique=True)
+    store_location = models.CharField(max_length=100)
+    store_created_date = models.DateTimeField(auto_now_add=True)
+    store_updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.store_id} - {self.store_location}"
+
+
+class Product(models.Model):
+    product_id = models.IntegerField(unique=True)
+    product_name = models.CharField(max_length=100)
+    product_created_date = models.DateTimeField(auto_now_add=True)
+    product_updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product_id} - {self.product_name}"
